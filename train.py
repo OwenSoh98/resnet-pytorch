9b4a67d129548067182bfef7a4484327367e7f27
OwenSoh98 <52849635+OwenSoh98@users.noreply.github.com>
2022-02-06 14:22:14 +0800

    Reduced BATCHSIZE from 1024 to 25, 0.8466 on test

--- train.py
+++ train.py
@@ -11,23 +11,25 @@ import csv
 
 class Train_Model():
     def __init__(self):
-        self.batch_size = 1024
-        self.device = torch.device(0 if torch.cuda.is_available() else 'cpu')
-        self.dataset = Classification_Dataset('./CIFAR-10/train', './CIFAR-10/val', './CIFAR-10/test', 'cifar10_mean_std.csv', imgsz=32, batch_size=self.batch_size, shuffle=True)
-        self.num_class = 10
-        self.epochs = 50
-        self.lr = 0.01
+        self.epochs = 30
+        self.batch_size = 256
+        self.lr = 0.05
         self.weight_decay = 0.0005
         self.momentum = 0.9
+        self.num_class = 10
+
+        self.device = torch.device(0 if torch.cuda.is_available() else 'cpu')
+        self.dataset = Classification_Dataset('./CIFAR-10/train', './CIFAR-10/val', './CIFAR-10/test', 'cifar10_mean_std.csv', imgsz=32, batch_size=self.batch_size, shuffle=True)
 
         self.modelpath = './results/' + datetime.now().strftime("%d-%m-%Y-%H-%M-%S") + '/'
         self.model = Resnet_Cifar10(input_size=32, num_class=self.num_class, n=2).to(self.device)
         self.loss_function = nn.CrossEntropyLoss()
         self.optimizer = optim.SGD(self.model.parameters(), self.lr, weight_decay=self.weight_decay, momentum=self.momentum)
         # self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[20, 40], gamma=0.1)
-        # self.scheduler = optim.lr_scheduler.OneCycleLR(self.optimizer, max_lr=0.1, epochs=40, steps_per_epoch=(self.dataset.train_len // self.batch_size))
-        self.scheduler = optim.lr_scheduler.OneCycleLR(self.optimizer, max_lr=0.05, epochs=self.epochs, steps_per_epoch=len(self.dataset.train_loader), pct_start=0.4, 
-                            anneal_strategy="linear", div_factor=0.05/self.lr, final_div_factor=self.lr/0.0005, three_phase=True)
+        self.scheduler = optim.lr_scheduler.OneCycleLR(self.optimizer, max_lr=0.1, epochs=self.epochs, steps_per_epoch=len(self.dataset.train_loader), pct_start=0.3, 
+                            anneal_strategy="cos", div_factor=0.1/self.lr)
+        # self.scheduler = optim.lr_scheduler.OneCycleLR(self.optimizer, max_lr=0.1, epochs=self.epochs, steps_per_epoch=len(self.dataset.train_loader), pct_start=0.4, 
+        #                     anneal_strategy="linear", div_factor=0.1/self.lr, final_div_factor=self.lr/0.005, three_phase=True)
         self.train()
 
     def create_directory(self, path):
